281fc899cdcbfddb5cb3d62fba7349299848bf95
Zhenyu Liao <maple01@126.com>
2017-09-01 19:20:50 +0800

    1. 引入定时任务模块

--- run.py
+++ run.py
@@ -3,9 +3,13 @@
 
 from tasks import index_page
 from util.dbutil import DBUtil
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 if __name__ == "__main__":
     db_util = DBUtil()
     db_util.init_db()
     index_page.index_page_spider()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(index_page.index_page_spider, 'interval', minutes=120)
+    scheduler.start()
